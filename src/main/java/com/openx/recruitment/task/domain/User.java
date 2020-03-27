dd92fb8d721093264d5dd8a05529f16b9972d16c
dziewonska.jolanta@gmail.com <123Magik1998>
2020-03-27 11:28:43 +0100

    code review

--- src/main/java/com/openx/recruitment/task/domain/User.java
+++ src/main/java/com/openx/recruitment/task/domain/User.java
@@ -12,6 +12,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    
     private int id;
     private String name;
     private String username;
--- src/main/java/com/openx/recruitment/task/domain/UserStats.java
+++ src/main/java/com/openx/recruitment/task/domain/UserStats.java
@@ -4,6 +4,7 @@ import lombok.Value;
 
 @Value
 public class UserStats {
+
     String username;
     int postsCount;
 
